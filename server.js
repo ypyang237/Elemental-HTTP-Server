863535ebab95f1defacf35d8f89aa87d1e559178
Pamela Yang <ypyang237@gmail.com>
2016-04-06 22:16:24 -1000

    finishes GET POST and PUT methods for http server, may be displayed on localhost 8080

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var qs = require('querystring');
 
 
 var server = http.createServer(function(request, response) {
@@ -35,30 +36,100 @@ var server = http.createServer(function(request, response) {
 
 
 
-  if(method === 'POST') {
-    console.log(headers);
+  if(method === 'POST' && url === '/elements') {
+
+    request.on('data', function(chunk) {
+
+    result = chunk.toString();
+    var parsed = qs.parse(result);
+
+    var elementName = parsed.elementName;
+    var elementSymbol = parsed.elementSymbol;
+    var elementAtomicNumber = parsed.elementAtomicNumber;
+    var elementDescription = parsed.elementDescription;
+
+    var myFile = fs.createWriteStream('public/' + elementName + '.html', { encoding: "utf8" }    );
+
+    myFile.write(
+      '<!DOCTYPE html>\r\n<html lang="en">\r\n<head>\r\n<meta charset="UTF-8">\r\n<title>\r\n'+
+      'The Elements - Helium</title>\r\n<link rel="stylesheet" href="/css/styles.css">\r\n'+
+      '</head>\r\n<body>\r\n'+
+      '<h1>' + elementName + '</h1>\r\n' +
+      '<h2>' + elementSymbol + '</h2>\r\n' +
+      '<h3>Atomic Number '+ elementAtomicNumber + '</h3>\r\n' +
+      '<p>' + elementDescription + '</p>\r\n' +
+      '<a href="/">back</a>\r\n</p></body></html>'
+      );
+
+    var index = fs.readFile('public/index.html', function(error, chunk) {
+       var chunky = chunk;
+       var addLink = (chunky.toString().substr(0, 258) +
+        '\r\n' + '<li>'+ '\r\n' + '<a href="/' + elementName + '.html">' + elementName + '</a>'+ '\r\n' + '</li>' +
+        chunky.toString().substr(258) );
+
+      var myFile = fs.createWriteStream('public/index.html');
+      myFile.write(addLink);
+
+      });
+
+    var responseBody = ({'success' : true});
+    response.write(JSON.stringify(responseBody));
+    response.statusCode = 200;
+
+    response.end();
+
+    });
+
+  }  //end of POST
+
+  if(method === 'PUT') {
+
+    request.on('data', function(chunk) {
+
+    result = chunk.toString();
+    var parsed = qs.parse(result);
+
+    var elementName = parsed.elementName;
+    var elementSymbol = parsed.elementSymbol;
+    var elementAtomicNumber = parsed.elementAtomicNumber;
+    var elementDescription = parsed.elementDescription;
+
+      fs.readFile('public/' + url, function(error, chunk) {
+      if(error) {
+        var failBody = { "error" : "resource /carbon.html does not exist" };
+        response.statusCode = 500;
+        return response.write(JSON.stringify(failBody));
+
+      } else {
+        var myFile = fs.createWriteStream('public' + url);
+
+        var successBody = { "success" : true };
+        response.write(JSON.stringify(successBody));
+        response.statusCode = 200;
+
+      myFile.write(
+      '<!DOCTYPE html>\r\n<html lang="en">\r\n<head>\r\n<meta charset="UTF-8">\r\n<title>\r\n'+
+      'The Elements - Helium</title>\r\n<link rel="stylesheet" href="/css/styles.css">\r\n'+
+      '</head>\r\n<body>\r\n'+
+      '<h1>' + elementName + '</h1>\r\n' +
+      '<h2>' + elementSymbol + '</h2>\r\n' +
+      '<h3>'+ elementAtomicNumber + '</h3>\r\n' +
+      '<p>' + elementDescription + '</p>\r\n'+
+      '<a href="/">back</a>\r\n\r\n</p></body></html>'
+      );
+
+
+      }
+
+       response.end();
+    });
+
+
+    });
+
+
+
   }
-
-
-
-
-
-  // request.on('error', function(err) {
-  //   // This prints the error message and stack trace to `stderr`
-  //   console.error(err);
-  // });
-
-  // request.on('data', function(data) {
-
-
-  //   console.log('request has been sent');
-
-  // });
-
-  // request.on('end', function(){
-
-  //   console.log('finished this request');
-  // });
 
 }); //end of http.createServer
 

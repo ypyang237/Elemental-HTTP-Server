b689443de86a29d04b9f47b6a290c90aafa8deec
Pamela Yang <ypyang237@gmail.com>
2016-04-07 18:01:41 -1000

    finishes authentication

--- server.js
+++ server.js
@@ -9,152 +9,199 @@ var server = http.createServer(function(request, response) {
   var method = request.method;
   var url = request.url;
 
+  var errorBody = null;
 
 
-  if(method === 'GET') {
+ if(request.rawHeaders.indexOf('Authorization') === -1  ) {
+    console.log('theres no authorization');
 
-    if(url === '/') {
-      fs.readFile('public/index.html', function(error, chunk) {
-        response.end(chunk);
-      });
-    } else {
-      fs.readFile('public' + url, function(error, chunk) {
+    response.setHeader('WWW-Authenticate', "BasicRealm = SecureArea");
+    return response.end(`<html><body>Not Authorized</body></html>`);
 
-        if(error) {
-          console.log('ERROR');
+  }
+  else {
+     console.log('Have authentication, could it be the right password? ');
 
-          return fs.readFile('public/404.html', function(error, chunk) {
+    var encodedString = request.headers.authorization.substr(6);  // QWxhZGRpbjpvcGVuc2VzYW1l
+    var base64Buffer = new Buffer(encodedString, 'base64');
+    var decodedString = base64Buffer.toString();
 
-            if(error) { console.log('cannot read 404 HTML'); }
+    if(decodedString.indexOf('Aladdin:opensesame')=== -1){
+      console.log('Nope, wrong, try again');
 
-            response.end(chunk);
-          });
-        }
-        return response.end(chunk);
-      });
+        response.writeHead(401, {
+          'WWW-Authenticate' : "BasicRealm = SecureArea"
+         });
+
+          return response.end(`<html><body>Not Authorized</body></html>`);
     }
-  }  //end of if method is get
 
+    console.log('It is Authorized', decodedString);
 
+    if(method === 'GET') {
 
-  if(method === 'POST' && url === '/elements') {
-
-    request.on('data', function(chunk) {
-
-    result = chunk.toString();
-    var parsed = qs.parse(result);
-
-    var elementName = parsed.elementName;
-    var elementSymbol = parsed.elementSymbol;
-    var elementAtomicNumber = parsed.elementAtomicNumber;
-    var elementDescription = parsed.elementDescription;
-
-    var myFile = fs.createWriteStream('public/' + elementName + '.html', { encoding: "utf8" }    );
-
-    myFile.write(
-      '<!DOCTYPE html>\r\n<html lang="en">\r\n<head>\r\n<meta charset="UTF-8">\r\n<title>\r\n'+
-      'The Elements - Helium</title>\r\n<link rel="stylesheet" href="/css/styles.css">\r\n'+
-      '</head>\r\n<body>\r\n'+
-      '<h1>' + elementName + '</h1>\r\n' +
-      '<h2>' + elementSymbol + '</h2>\r\n' +
-      '<h3>Atomic Number '+ elementAtomicNumber + '</h3>\r\n' +
-      '<p>' + elementDescription + '</p>\r\n' +
-      '<a href="/">back</a>\r\n</p></body></html>'
-      );
-
-    var index = fs.readFile('public/index.html', function(error, chunk) {
-       var chunky = chunk;
-       var addLink = (chunky.toString().substr(0, 258) +
-        '\r\n' + '<li>'+ '\r\n' + '<a href="/' + elementName + '.html">' + elementName + '</a>'+ '\r\n' + '</li>' +
-        chunky.toString().substr(258) );
-
-      var myFile = fs.createWriteStream('public/index.html');
-      myFile.write(addLink);
-
-      });
-
-    var responseBody = ({'success' : true});
-    response.write(JSON.stringify(responseBody));
-    response.statusCode = 200;
-
-    response.end();
-
-    });
-
-  }  //end of POST
-
-  if(method === 'PUT') {
-
-    request.on('data', function(chunk) {
-
-    result = chunk.toString();
-    var parsed = qs.parse(result);
-
-    var elementName = parsed.elementName;
-    var elementSymbol = parsed.elementSymbol;
-    var elementAtomicNumber = parsed.elementAtomicNumber;
-    var elementDescription = parsed.elementDescription;
-
-      fs.readFile('public/' + url, function(error, chunk) {
-      if(error) {
-        var failBody = { "error" : "resource /carbon.html does not exist" };
-        response.statusCode = 500;
-        return response.write(JSON.stringify(failBody));
-
+      if(url === '/') {
+        fs.readFile('public/index.html', function(error, chunk) {
+          response.end(chunk);
+        });
       } else {
-          var myFile = fs.createWriteStream('public' + url);
+        fs.readFile('public' + url, function(error, chunk) {
 
-          var successBody = { "success" : true };
-          response.write(JSON.stringify(successBody));
-          response.statusCode = 200;
+          if(error) {
+            return fs.readFile('public/404.html', function(error, chunk) {
 
-          myFile.write(
+              if(error) { console.log('cannot read 404 HTML'); }
+
+              response.end(chunk);
+            });
+          }
+          return response.end(chunk);
+        });
+      }
+    }  //end of if method is get
+
+
+
+    if(method === 'POST' && url === '/elements') {
+
+      request.on('data', function(chunk) {
+
+        result = chunk.toString();
+        var parsed = qs.parse(result);
+
+        //fs.readFile('templates/elementTemplate.html', (err, template) => {
+        // console.log(template.toString());
+        // if(err) {
+        //    throw new Error(err)
+        //  }
+        // const rendedredTemplate = templateHelper.element( template, data.elementName, data.elementSymbo, data.elementNumber, data.elementDescription);
+        //console.log(renderedTemplate)   -> have to be put in the fs.readFile bracket coz of asynchronous programming
+        //})
+        // we shall parse thru this file and inject all the data we want to display
+        // fs.writeFile('public/' + data.elementName + '.html', renderedTemplate, (err) => {
+        //  if(err) {
+        //    throw new Error
+        //  }
+        //  response.writeHead(200, {
+        //   'Content-Type' : 'application/json'
+        //  })
+        //  response.end(JSON.stringify({ 'success' : true}))
+        // }
+
+        var elementName = parsed.elementName;
+        var elementSymbol = parsed.elementSymbol;
+        var elementAtomicNumber = parsed.elementAtomicNumber;
+        var elementDescription = parsed.elementDescription;
+
+        var myFile = fs.createWriteStream('public/' + elementName + '.html', { encoding: "utf8" }    );
+
+        myFile.write(
           '<!DOCTYPE html>\r\n<html lang="en">\r\n<head>\r\n<meta charset="UTF-8">\r\n<title>\r\n'+
           'The Elements - Helium</title>\r\n<link rel="stylesheet" href="/css/styles.css">\r\n'+
           '</head>\r\n<body>\r\n'+
           '<h1>' + elementName + '</h1>\r\n' +
           '<h2>' + elementSymbol + '</h2>\r\n' +
-          '<h3>'+ elementAtomicNumber + '</h3>\r\n' +
-          '<p>' + elementDescription + '</p>\r\n'+
-          '<a href="/">back</a>\r\n\r\n</p></body></html>'
+          '<h3>Atomic Number '+ elementAtomicNumber + '</h3>\r\n' +
+          '<p>' + elementDescription + '</p>\r\n' +
+          '<a href="/">back</a>\r\n</p></body></html>'
           );
 
+        var index = fs.readFile('public/index.html', function(error, chunk) {
+           var chunky = chunk;
+           var addLink = (chunky.toString().substr(0, 258) +
+            '\r\n' + '<li>'+ '\r\n' + '<a href="/' + elementName + '.html">' + elementName + '</a>'+ '\r\n' + '</li>' +
+            chunky.toString().substr(258) );
 
-        }
+          var myFile = fs.createWriteStream('public/index.html');
+          myFile.write(addLink);
+
+          });
+
+        var responseBody = ({'success' : true});
+        response.write(JSON.stringify(responseBody));
+        response.statusCode = 200;
+
         response.end();
 
       });
 
-    });
+      request.on('end', function() {
+        //VALIDATION CHECK, if it === 'undefined'
+      });
 
-  } //end of PUT request
+    }  //end of POST
+
+    if(method === 'PUT') {
+
+      request.on('data', function(chunk) {
+
+      result = chunk.toString();
+      var parsed = qs.parse(result);
+
+      var elementName = parsed.elementName;
+      var elementSymbol = parsed.elementSymbol;
+      var elementAtomicNumber = parsed.elementAtomicNumber;
+      var elementDescription = parsed.elementDescription;
+
+        fs.readFile('public/' + url, function(error, chunk) {
+        if(error) {
+          var failBody = { "error" : "resource /carbon.html does not exist" };
+          response.statusCode = 500;
+          return response.write(JSON.stringify(failBody));
+
+        } else {
+            var myFile = fs.createWriteStream('public' + url);
+
+            var successBody = { "success" : true };
+            response.write(JSON.stringify(successBody));
+            response.statusCode = 200;
+
+            myFile.write(
+            '<!DOCTYPE html>\r\n<html lang="en">\r\n<head>\r\n<meta charset="UTF-8">\r\n<title>\r\n'+
+            'The Elements - Helium</title>\r\n<link rel="stylesheet" href="/css/styles.css">\r\n'+
+            '</head>\r\n<body>\r\n'+
+            '<h1>' + elementName + '</h1>\r\n' +
+            '<h2>' + elementSymbol + '</h2>\r\n' +
+            '<h3>'+ elementAtomicNumber + '</h3>\r\n' +
+            '<p>' + elementDescription + '</p>\r\n'+
+            '<a href="/">back</a>\r\n\r\n</p></body></html>'
+            );
+
+          }
+          response.end();
+
+        });
+
+      });
+
+    } //end of PUT request
 
 
-  if(method === 'DELETE') {
+    if(method === 'DELETE') {
 
-    fs.readFile('public/' + url, function(error, chunk) {
-      if(error) {
-        var failBody =  { "error" : "resource " + url + "does not exist" };
-        console.log(gutil.colors.red('File not found, so not deleting.'));
-        response.statusCode = 500;
-        return response.write(JSON.stringify(failBody));
-      }
-      else {
-        response.statusCode = 200;
-        var successBody = { "success" : true };
-        console.log(gutil.colors.green('File exists. Deleting now ...'));
-        fs.unlink('./public' + url);
-        return response.write(JSON.stringify(successBody));
-      }
+      fs.readFile('public/' + url, function(error, chunk) {
+        if(error) {
+          var failBody =  { "error" : "resource " + url + " does not exist" };
+          console.log(gutil.colors.red('File not found, so not deleting.'));
+          response.statusCode = 500;
+          response.write(JSON.stringify(failBody));
+          return response.end();
+        }
+        else {
+          response.statusCode = 200;
+          var successBody = { "success" : true };
+          console.log(gutil.colors.green('File exists. Deleting now ...'));
+          fs.unlink('./public' + url);
+          response.write(JSON.stringify(successBody));
+          return response.end();
 
-    });
+        }
 
+      });
 
+    }//end of DELETE request
 
-  }//end of DELETE request
-
-
-
+  }
 
 
 
---
+++ templates/templateHelper.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports.elementBuilder = ( template, name, symbol, number, desc) => {
+ return template.toString()
+  .replace(/{{ elementName }}/gi, name)
+  .replace(/{{ elementSymbol }}/gi, symbol)
+  .replace(/{{ elementAtomicNumber }}/gi, number)
+  .replace(/{{ elementDescription }}/gi, desc)
+
+
+
+}
